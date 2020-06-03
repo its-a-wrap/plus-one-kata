c25384594fdb259c2b300ed865db20a6091a1c98
its-a-wrap <patrick_tweed@hotmail.co.uk>
2020-06-03 13:36:06 +0100

    working solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 const plusOne = digits => {
-
+    const number = parseInt(digits.join('')) + 1;
+    return number.toString().split('').map(string => parseInt(string));
 };
 
 module.exports = plusOne;
--- src/index.spec.js
+++ src/index.spec.js
@@ -2,5 +2,19 @@ const { expect } = require('chai');
 const plusOne = require('./index');
 
 describe('GIVEN plusOne function', () => {
+    describe('AND the input is an array of integers representing an integer', () => {
+        it('SHOULD add one to the integer and return it represented as an array', () => {
+            const inputArray = [1, 2, 3];
+            const output = plusOne(inputArray);
 
+            expect(output).to.be.eql([1, 2, 4]);
+        });
+
+        it('SHOULD add one to the integer and return it represented as an array', () => {
+            const inputArray = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3];
+            const output = plusOne(inputArray);
+
+            expect(output).to.be.eql([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]);
+        });
+    });
 });
